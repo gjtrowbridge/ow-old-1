57e1be5cbd31c495563c022d3ca4b18fc49b6ce4
Greg Trowbridge <gjtrowbridge@gmail.com>
2011-08-02 16:40:13 -0700

    Removed old mailer test code, updated nav bar with devise helpers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,4 @@ class StaticPagesController < ApplicationController
 	def index
 		render :layout => 'homepage'
 	end
-	def send_mail
-		UserMailer.test.deliver
-		flash[:notice] = 'mail sent'
-		redirect_to(root_path)
-	end
 end
